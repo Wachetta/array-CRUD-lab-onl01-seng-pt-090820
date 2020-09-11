575d34bd0c5cbef0cbad1a3d9eecb3e3a7afd45c
Zachary Darneille <zakdarn@gmail.com>
2020-09-11 18:49:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,17 +12,18 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  pets = ["cat", "dog", "hamster", "parrot"] #this is only referenced in the scope of this method, and is why I have to define it again
-   pets.unshift("wow")#add wow to front of array
+ array = ["I", "am", "really", "learning"] #this is only referenced in the scope of this method, and is why I have to define it again
+   array.unshift("wow")#add wow to front of array
 end
 
 def remove_element_from_end_of_array(array)
-  pets = ["cat", "dog", "hamster", "parrot"] #this is only referenced in the scope of this method, and is why I have to define it again
-    pets.pop #remove the last element (parrot) of the array,
+  array = ["I", "am", "really", "learning", "arrays!"] #this is only referenced in the scope of this method, and is why I have to define it again
+    array.pop #remove the last element (parrot) of the array,
 end
 
 def remove_element_from_start_of_array(array)
-
+ array = ["wow", "I", "am", "really", "learning", "arrays!"] #it "takes arguments of an array and an element and adds that element to the end of the array"
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
