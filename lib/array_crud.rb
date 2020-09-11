7d76034eee000a26a8cd13e78530311f11f25d41
Zachary Darneille <zakdarn@gmail.com>
2020-09-11 18:41:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,14 +6,14 @@ def create_an_array
  ["cat", "dog", "hamster", "parrot"] # create an array with four elements
 end
 
-pets = ["cat", "dog", "hamster", "parrot"]
-
 def add_element_to_end_of_array(array, element)
+ pets = ["cat", "dog", "hamster", "parrot"] #this is only referenced in the scope of this method, and is why I have to define it again
    pets << "arrays!" # add arrays! to the back of an array, used shovel method
 end
 
 def add_element_to_start_of_array(array, element)
-#add wow to front of array
+ pets = ["cat", "dog", "hamster", "parrot"] #this is only referenced in the scope of this method, and is why I have to define it again
+   pets.unshift("wow")#add wow to front of array
 end
 
 def remove_element_from_end_of_array(array)
